e5cefebf788be6e3d4aad337ebc5694f414d61e3
Alisher <alishktl99@gmail.com>
2020-07-03 12:36:28 +0600

    Initial Commit

---
+++ QonilQui/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QonilQui
+//
+//  Created by Alisher Sattarbek on 7/3/20.
+//  Copyright © 2020 AlisherSattarbek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
