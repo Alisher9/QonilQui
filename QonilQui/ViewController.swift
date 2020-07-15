936f10686f099befc5613e85c82f4daa96747897
Alisher <alishktl99@gmail.com>
2020-07-15 13:13:35 +0600

    scrollViewDelegate added for swiping

--- QonilQui/ViewController.swift
+++ QonilQui/ViewController.swift
@@ -8,26 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class ViewController: UIViewController, UIScrollViewDelegate{
 
-    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    let arr = ["Welcome1", "Welcome2", "Welcome3"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.delegate = self
-        collectionView.dataSource = self
+        
+        pageControl.numberOfPages = arr.count
+        
+        for i in 0..<arr.count {
+            let imageView = UIImageView()
+            imageView.contentMode = .scaleToFill
+            imageView.image = UIImage(named: arr[i])
+            let xPos = CGFloat(i)*self.view.bounds.size.width
+            imageView.frame = CGRect(x: xPos, y:0, width: view.frame.size.width, height: scrollView.frame.size.height)
+            scrollView.contentSize.width = view.frame.size.width*CGFloat(i+1)
+            scrollView.addSubview(imageView)
+        }
     }
-    
-    
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let page = scrollView.contentOffset.x / scrollView.frame.width
+        
+        pageControl.currentPage = Int(page)
     }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "WelcomeCollectionViewCell", for: indexPath) as! WelcomeCollectionViewCell
-        cell.imageView.image = UIImage(named: "")
-        return cell
-    }
-    
-    
+
 }
